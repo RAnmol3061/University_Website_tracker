5a1acf38b43e6087d303bdaf54eb4973a3444be6
R_Anmol <anmolmain3061@gmail.com>
2026-02-28 21:55:16 +0530

    Scrapes scheme and syllabus link

--- unitracker/unitracker/spiders/syllabustracker.py
+++ unitracker/unitracker/spiders/syllabustracker.py
@@ -4,7 +4,22 @@ import scrapy
 class SyllabustrackerSpider(scrapy.Spider):
     name = "syllabustracker"
     allowed_domains = ["csvtu.ac.in"]
-    start_urls = ["https://csvtu.ac.in"]
+    start_urls = ["https://csvtu.ac.in/ew/programs-and-schemes/"]
 
     def parse(self, response):
-        pass
+        left_block = response.css('div.col-md-6')
+        btech = left_block.xpath('.//a[normalize-space(.)="Bachelor of Technology (B.Tech.) with effective from session 2025-26 (for students enrolled from the year 2025-26)"]/ancestor::div[2]')
+        final = btech.xpath(".//table//tr")
+
+        for i in final:
+            data = i.xpath(".//td").getall()
+            yield {
+                "Course Name": data[0],
+                "Scheme-Link": data[1],
+                "Syllabus-Link": data[2],
+            }
+
+        
+
+
+            
