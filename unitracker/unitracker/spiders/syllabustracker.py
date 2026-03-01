ede14ca6a8c87225909d4351b52c5f70fd7c2b46
R_Anmol <anmolmain3061@gmail.com>
2026-03-01 12:50:57 +0530

    Scrapes both scheme link and individual semester links

--- unitracker/unitracker/spiders/syllabustracker.py
+++ unitracker/unitracker/spiders/syllabustracker.py
@@ -12,14 +12,34 @@ class SyllabustrackerSpider(scrapy.Spider):
         final = btech.xpath(".//table//tr")
 
         for i in final:
-            data = i.xpath(".//td").getall()
-            yield {
-                "Course Name": data[0],
-                "Scheme-Link": data[1],
-                "Syllabus-Link": data[2],
-            }
+            data = i.xpath(".//td")
 
-        
-
+            absolute_link = "https://csvtu.ac.in/ew/" + (data[2].css('a::attr(href)').get())[2:]
+            
+            yield response.follow(absolute_link,
+                                  callback=self.parse_syllabus_page,
+                                  meta = {"Course Name": data[0].css('td::text').get(),
+                                          "Scheme-Link": data[1].css('a::attr(href)').get()})
 
             
+
+        
+    def parse_syllabus_page(self,response):
+        left_block = response.css('div.col-md-6')
+        syllabus = left_block.css('div.syllabus-content')
+        rows = syllabus.xpath('.//tr[td]')
+        group = {}
+
+        for i in rows:
+            which_sem = i.xpath('.//a[@class="package-title"]/text()').get()
+            upload_date = i.xpath('.//span[@class="__dt_update_date "]/text()').get()
+            syllabus_link = i.css('a.wpdm-download-link.download-on-click.btn.btn-primary::attr(data-downloadurl)').get()
+
+            group[which_sem] = [response.meta["Course Name"], response.meta["Scheme-Link"], syllabus_link, upload_date]
+        
+        yield group
+            
+        
+
+            
+        
