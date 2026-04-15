5d30dd902e2611ea5c59a4d7a598330fdb237dda
R_Anmol <anmolmain3061@gmail.com>
2026-04-15 17:21:14 +0530

    perf: simplify spider throttle settings
    - Remove hard limits and let Autothrottle handle them
    - Remove boilerplate comments

--- unitracker/unitracker/settings.py
+++ unitracker/unitracker/settings.py
@@ -22,24 +22,19 @@ SHELL = 'ipython'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
-# Concurrency and throttling settings
 CONCURRENT_REQUESTS = 8
-CONCURRENT_REQUESTS_PER_DOMAIN = 2
-DOWNLOAD_DELAY = 2
+RANDOM_DOWNLOAD_DELAY = True
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+
 AUTOTHROTTLE_ENABLED = True
-# The initial download delay
 AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
 AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 2.0
+
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
-RANDOM_DOWNLOAD_DELAY = True
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
